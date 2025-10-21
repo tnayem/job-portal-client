e64e24e23dc49f64c2da60953f925b8d7f1b141f
tnayem <tasdidnayem@gmail.com>
2025-10-21 09:57:30 +0600

    implement login page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Animation from '../../components/Animation';
 
 const Home = () => {
     return (
         <div>
-            
+            <Animation></Animation>
         </div>
     );
 };
